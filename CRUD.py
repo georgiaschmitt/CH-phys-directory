e0a63760b3ae63d1b96006cb59045f461838dcad
georgiaschmitt <georgia.schmitt@gmail.com>
2020-11-07 20:22:06 +0000

    CRUD additions

--- CRUD.py
+++ CRUD.py
@@ -10,11 +10,12 @@ def create_user(email, password):
     db.session.commit()
     return user
 
-def add_favorite(user, physician):
-    """Create and return a new favorite."""
-    
-    user.favorites.append(physician)
+def create_location(institution, city, state, coordinates):
+    """Create and return a new location in the database."""
+    location = Location(institution=institution, city=city, state=state, coordinates=coordinates)
+    db.session.add(location)
     db.session.commit()
+    return location
 
 def create_physician(name, location):
     """Create and return a physician."""
@@ -23,6 +24,60 @@ def create_physician(name, location):
     db.session.add(physician)
     db.session.commit()
     return physician
+
+def add_favorite(user, physician):
+    """Create and return a new favorite."""
+    
+    user.favorites.append(physician)
+    db.session.commit()
+    return f"Added favorite: {physician}!"
+
+def get_all_physicians():
+    """Returns all physicians."""
+    return Physician.query.all()
+
+def get_all_users():
+    """Returns all users."""
+    return User.query.all()
+
+def get_all_locations():
+    """Returns all locations."""
+    return Location.query.all()
+
+def get_physician_by_id(physician_id):
+    """Return physician by id."""
+    return Physician.query.get(physician_id)
+
+def get_user_by_id(user_id):
+    """Return user by id."""
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return user by email."""
+    return User.query.filter(User.email == email).first()
+
+def get_physician_by_name(name):
+    """Return physicians by name."""
+    return Physician.query.filter(Physician.name==name).all()
+
+def get_physician_by_city(city):
+    """Return physician by city."""
+    return Physician.query.filter(Physician.location.city == city).all()
+
+def get_physician_by_state(state):
+    """Return physician by state."""
+    return Physician.query.filter(Physician.location.state = state).all()
+
+def password_match(email, password):
+    """Check if password matches email."""
+    return User.query.filter(User.email == email, User.password==password).first()
+
+def get_location_by_id(location_id):
+    """Return location by id."""
+    return Location.query.get(location_id)
+
+
+
 
 
 
